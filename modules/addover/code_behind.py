72f0d50bb5c810a3f916f06fb6b6a36562bfe8f5
JL710 <git@jl710.de>
2022-07-24 16:43:37 +0200

    Update code_behind.py
    bug fix and test curutine(start()) for ui testing

--- modules/addover/code_behind.py
+++ modules/addover/code_behind.py
@@ -4,19 +4,19 @@ from pathlib import Path
 
 def load_target() -> Path:
     with open(Path("modules", "addover", "data.json"), "r") as f:
-        path_string = json.load(f)["lastTarget"]     
+        path_string = json.load(f)["Target"]     
     path = Path(path_string)
-    if path_string == "" or not path.is_dir():
+    if path_string == "":
         return Path.home()
     return path
 
 def set_target(path: str):
     with open(Path("modules", "addover", "data.json"), "r") as f:
         data = json.load(f)
-    data["lastTarget"] = path
+    data["Target"] = path
     with open(Path("modules", "addover", "data.json"), "w") as f:
         json.dump(data, f)
-        
+
 def load_paylods() -> list:
     with open(Path("modules", "addover", "data.json"), "r") as f:
         data = json.load(f)
@@ -44,3 +44,10 @@ def remove_payload(payload: str):
         data["payload"].remove(payload)
         with open(Path("modules", "addover", "data.json"), "w") as f:
             json.dump(data, f)
+
+def start():
+    import time
+    for i in range(0, 101, 10):
+        yield {"event-message": f"event: {i}", "percentage": i, "finished": False if i != 100 else True}
+        time.sleep(1)
+
