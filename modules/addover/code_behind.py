a37d1ffce98f1a1f031cb5dc40ad2197bd8f46e7
JL710 <jan.lyncker@freenet.de>
2022-07-31 20:02:11 +0200

    addover profiles
    beginn with profile implementation for addover module

--- modules/addover/code_behind.py
+++ modules/addover/code_behind.py
@@ -5,77 +5,81 @@ import os
 
 
 
-def load_target() -> Path:
+def load_data() -> dict:
     with open(Path("modules", "addover", "data.json"), "r") as f:
-        path_string = json.load(f)["Target"]     
+        data = json.load(f)
+    return data
+
+def write_data(data: dict):
+    with open(Path("modules", "addover", "data.json"), "w") as f:
+        json.dump(data, f)
+
+
+# TODO: check profile decorator
+
+
+def get_target(profile: str) -> Path:
+    path_string = load_data()["profiles"][profile]["target"]
     path = Path(path_string)
     if path_string == "":
         return Path.home()
     return path
 
-def set_target(path: str):
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
-    data["Target"] = path
-    with open(Path("modules", "addover", "data.json"), "w") as f:
-        json.dump(data, f)
+def set_target(profile: str, path: str):
+    data = load_data()
+    data["profiles"][profile]["target"] = path
+    write_data(data)
 
-def load_paylods() -> list:
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
-    return data["payload"]
+def get_profile_names() -> list:
+    data = load_data()
+    return list(data["profiles"].keys())
 
-def set_payload(payload: list):
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
-    data["payload"] = payload
-    with open(Path("modules", "addover", "data.json"), "w") as f:
-        json.dump(data, f)
+def get_payloads(profile: str) -> list:
+    data = load_data()
+    return data["profiles"][profile]["payload"]
 
-def add_payload(payload: str):
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
-    if not payload in data["payload"] and Path(payload).exists():
-        data["payload"].append(payload)
-        with open(Path("modules", "addover", "data.json"), "w") as f:
-            json.dump(data, f)
+def set_payload(profile: str, payload: list):
+    data = load_data()
+    data["profiles"][profile]["payload"] = payload
+    write_data(data)
 
-def remove_payload(payload: str):
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
-    if payload in data["payload"]:
-        data["payload"].remove(payload)
-        with open(Path("modules", "addover", "data.json"), "w") as f:
-            json.dump(data, f)
+def add_payload(profile: str, payload: str):
+    data = load_data()
+    if not payload in data["profiles"][profile]["payload"] and Path(payload).exists():
+        data["profiles"][profile]["payload"].append(payload)
+        write_data(data)
+
+def remove_payload(profile: str, payload: str):
+    data = load_data()
+    if payload in data["profiles"][profile]["payload"]:
+        data["profiles"][profile]["payload"].remove(payload)
+        write_data(data)
 
 def get_command() -> str:
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
+    data = load_data()
     return data["command"]
 
 def set_command(command: str) -> str:
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
+    data = load_data()
     data["command"] = command
     with open(Path("modules", "addover", "data.json"), "w") as f:
         json.dump(data, f)
 
 def reset_command():
-    with open(Path("modules", "addover", "data.json"), "r") as f:
-        data = json.load(f)
+    data = load_data()
     data["command"] = data["default-command"]
     with open(Path("modules", "addover", "data.json"), "w") as f:
         json.dump(data, f)
 
-def start():
+def start(profile: str):
     # check if destination exists
-    if not Path(load_target()).exists():
-        yield {"event-message": f"event: Destination {load_target()} does not exist", "percentage": 100, "finished": True}
+    if not Path(get_target(profile)).exists():
+        yield {"event-message": f"event: Destination {get_target(profile)} does not exist", "percentage": 100, "finished": True}
         return
 
-    percent_factor = 100 / len(load_paylods())
+    percent_factor = 100 / len(get_payloads(profile))
 
-    for index_payload, payload in enumerate(load_paylods()):
+    for index_payload, payload in enumerate(get_payloads(profile)):
         # check if payload exists
         if not Path(payload).exists():
             yield {"event-message": f"event: Payload {payload} does not exist", "percentage": int(index_payload * percent_factor), "finished": False}
@@ -84,7 +88,7 @@ def start():
         yield {"event-message": f"Begin with: {payload}", "percentage": int(index_payload * percent_factor), "finished": False}
 
         command = get_command().replace("SOURCE", str(Path(payload)))
-        command = command.replace("DESTINATION", str(Path(load_target() / Path(payload).name)))
+        command = command.replace("DESTINATION", str(Path(get_target(profile) / Path(payload).name)))
         output = os.popen(command).read()
         yield {"event-message": f"xcopy output: {output}", "percentage": int(index_payload * percent_factor), "finished": False}
         
--- modules/addover/module.py
+++ modules/addover/module.py
@@ -1,25 +1,25 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 import modules.addover.code_behind as cb
 from pathlib import Path
 
 
-class Menu(QtWidgets.QMenu):
+class Menu(qtw.QMenu):
     def __init__(self, main_widget):
         super().__init__("Add Over")
 
-        self.__edit_command_action = QtWidgets.QAction("Edit Command")
+        self.__edit_command_action = qtw.QAction("Edit Command")
         self.__edit_command_action.triggered.connect(self.__on_edit_command)
 
-        self.__reset_command_action = QtWidgets.QAction("Reset Command")
+        self.__reset_command_action = qtw.QAction("Reset Command")
         self.__reset_command_action.triggered.connect(self.__on_reset_command)
 
         self.addAction(self.__edit_command_action)
         self.addAction(self.__reset_command_action)
 
     def __on_edit_command(self):
-        text, submit = QtWidgets.QInputDialog.getText(self, "Edit Command", "Edit Command here:", text=cb.get_command())
+        text, submit = qtw.QInputDialog.getText(self, "Edit Command", "Edit Command here:", text=cb.get_command())
         if submit:
             cb.set_command(text)
 
@@ -28,21 +28,24 @@ class Menu(QtWidgets.QMenu):
 
 
 
-class MainWidget(QtWidgets.QWidget):
+class MainWidget(qtw.QWidget):
+    # TODO: Property for current profile
+
+
     def __init__(self):
         super().__init__()
 
         # Stack shit
         self.__startup_widget = SetupWidget()
         self.__action_widget = ActionWidget()
-        self.__stack_widget = QtWidgets.QStackedWidget(self)
+        self.__stack_widget = qtw.QStackedWidget(self)
         self.__stack_widget.addWidget(self.__startup_widget)
         self.__stack_widget.addWidget(self.__action_widget)
 
         self.__stack_widget.setCurrentIndex(0)
 
         # do layout for MainWidget
-        self.layout = QtWidgets.QVBoxLayout()
+        self.layout = qtw.QVBoxLayout()
         self.layout.addWidget(self.__stack_widget)
         self.setLayout(self.layout)
 
@@ -50,135 +53,171 @@ class MainWidget(QtWidgets.QWidget):
         self.__startup_widget.submit.connect(self.__switch)
         self.__action_widget.exit.connect(self.__switch)
 
-    def __switch(self):
+    def __switch(self, profile: str=None):
         # swich stack
         if self.__stack_widget.currentIndex() == 0:
             self.__stack_widget.setCurrentIndex(1)
-            self.__action_widget.start.emit()        
+            self.__action_widget.start.emit(profile)        
         else:
             self.__stack_widget.setCurrentIndex(0)
 
 
 
-class SetupWidget(QtWidgets.QWidget):
+class SetupWidget(qtw.QWidget):
     # signal
-    submit = qtc.pyqtSignal()
+    submit = qtc.pyqtSignal(str)
     
+    current_profile = cb.get_profile_names()[0]
+
     def __init__(self):
         super().__init__()
 
-        # target widgets
-        self.__target_group_bot = self.TargetGroupBox()
+        # widgets
+        self.__profile_group_box = self.ProfileGroupBox()
+
+        self.__target_group_box = self.TargetGroupBox()
         
         self.__payload_list = self.PayloadList()
 
-        self.__start_button = QtWidgets.QPushButton("Start")
+        self.__start_button = qtw.QPushButton("Start")
         self.__start_button.clicked.connect(self.on_start)
-        self.__add_dir_payload_button = QtWidgets.QPushButton("Add Directory")
+        self.__add_dir_payload_button = qtw.QPushButton("Add Directory")
         self.__add_dir_payload_button.clicked.connect(self.__payload_list.add_dir)
-        self.__add_file_payload_button = QtWidgets.QPushButton("Add File")
+        self.__add_file_payload_button = qtw.QPushButton("Add File")
         self.__add_file_payload_button.clicked.connect(self.__payload_list.add_file)
-        self.__remove_payload_button = QtWidgets.QPushButton("Remove")
+        self.__remove_payload_button = qtw.QPushButton("Remove")
         self.__remove_payload_button.clicked.connect(self.__payload_list.remove)
 
         # layout
-        self.layout = QtWidgets.QGridLayout()
-        self.layout.addWidget(self.__target_group_bot, 0, 0, 1, 2)
-        self.layout.addWidget(self.__payload_list, 1, 0, 1, 2)
-        self.layout.addWidget(self.__remove_payload_button, 2, 0, 1, 1)
-        self.layout.addWidget(self.__add_dir_payload_button, 2, 1, 1, 1)
-        self.layout.addWidget(self.__add_file_payload_button, 3 ,1 , 1, 1)
-        self.layout.addWidget(self.__start_button, 3, 0, 1, 1)
+        self.layout = qtw.QGridLayout()
+        self.layout.addWidget(self.__profile_group_box, 0, 0, 1, 2)
+        self.layout.addWidget(self.__target_group_box, 1, 0, 1, 2)
+        self.layout.addWidget(self.__payload_list, 2, 0, 1, 2)
+        self.layout.addWidget(self.__remove_payload_button, 3, 0, 1, 1)
+        self.layout.addWidget(self.__add_dir_payload_button, 3, 1, 1, 1)
+        self.layout.addWidget(self.__add_file_payload_button, 4 ,1 , 1, 1)
+        self.layout.addWidget(self.__start_button, 4, 0, 1, 1)
         self.setLayout(self.layout)
 
     @qtc.pyqtSlot()
     def on_start(self):
-        if not cb.load_target().is_dir():
-            error = QtWidgets.QMessageBox()
+        if not cb.get_target(SetupWidget.current_profile).is_dir():
+            error = qtw.QMessageBox()
             error.setWindowTitle("Error")
             error.setText("Target does not exist!")
-            error.setIcon(QtWidgets.QMessageBox.Critical)
+            error.setIcon(qtw.QMessageBox.Critical)
             error.exec_()
             return
-        qm = QtWidgets.QMessageBox
-        shure = qm.question(self, "Make Shure", f'Is the Target "{str(cb.load_target())}" correct?', qm.Yes | qm.No)
+        qm = qtw.QMessageBox
+        shure = qm.question(self, "Make Shure", f'Is the Target "{str(cb.get_target(SetupWidget.current_profile))}" correct?', qm.Yes | qm.No)
         if shure == qm.Yes:  # the pass is validated by the user
-            self.submit.emit()
+            self.submit.emit(SetupWidget.current_profile)
 
 
-    class PayloadList(QtWidgets.QListWidget):
+    class PayloadList(qtw.QListWidget):
         def __init__(self):
             super().__init__()
             self.refresh()
 
         def refresh(self):
             self.clear()
-            for load in cb.load_paylods():
+            for load in cb.get_payloads(SetupWidget.current_profile):
                 self.addItem(load)
 
         @qtc.pyqtSlot()
         def add_dir(self):
-            path = QtWidgets.QFileDialog.getExistingDirectory()
+            path = qtw.QFileDialog.getExistingDirectory()
             if path != "":  # checks if user canceled selection without any select
-                cb.add_payload(path)
+                cb.add_payload(SetupWidget.current_profile, path)
             self.refresh()
 
         @qtc.pyqtSlot()
         def add_file(self):
-            path = QtWidgets.QFileDialog.getOpenFileName()
+            path = qtw.QFileDialog.getOpenFileName()
             print(path[0])
             if Path(path[0]).is_file():
-                cb.add_payload(path[0])
+                cb.add_payload(SetupWidget.current_profile, path[0])
             self.refresh()
 
         @qtc.pyqtSlot()
         def remove(self):
             item = self.currentItem()
             if not item == None:
-                cb.remove_payload(item.text())
+                cb.remove_payload(SetupWidget.current_profile, item.text())
             self.refresh()
 
-    class TargetGroupBox(QtWidgets.QGroupBox):
+    class TargetGroupBox(qtw.QGroupBox):
         def __init__(self):
             super().__init__()
             self.setTitle("Target")
 
-            self.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Maximum))
+            self.setSizePolicy(qtw.QSizePolicy(qtw.QSizePolicy.Preferred, qtw.QSizePolicy.Maximum))
 
-            self.__target = cb.load_target()
+            self.__target = cb.get_target(SetupWidget.current_profile)
 
             # widgets
-            self.__status_label = QtWidgets.QLabel(str(self.__target))
-            self.__change_button = QtWidgets.QPushButton("Change")
+            self.__status_label = qtw.QLabel(str(self.__target))
+            self.__vstretch_sizePolicy = qtw.QSizePolicy(qtw.QSizePolicy.Minimum, qtw.QSizePolicy.Fixed)
+            self.__vstretch_sizePolicy.setHorizontalStretch(1)
+            self.__status_label.setSizePolicy(self.__vstretch_sizePolicy)
+            self.__change_button = qtw.QPushButton("Change")
             self.__change_button.clicked.connect(self.on_change_target)
 
             # layout
-            self.__layout = QtWidgets.QHBoxLayout()
+            self.__layout = qtw.QHBoxLayout()
             self.__layout.addWidget(self.__status_label)
             self.__layout.addWidget(self.__change_button)
             self.setLayout(self.__layout)
 
         @qtc.pyqtSlot()
         def on_change_target(self):
-            path = Path(QtWidgets.QFileDialog.getExistingDirectory(self, "New Target"))
+            path = Path(qtw.QFileDialog.getExistingDirectory(self, "New Target"))
             if not path.is_absolute():
                 pass # needs to be checked -> if user cancels the selection process
             elif path.is_dir():
                 self.__target = path
                 self.__status_label.setText(str(self.__target))
-                cb.set_target(str(self.__target))
+                cb.set_target(SetupWidget.current_profile, str(self.__target))
             else:
-                error = QtWidgets.QMessageBox()
+                error = qtw.QMessageBox()
                 error.setWindowTitle("Error")
                 error.setText("Path does not exist!")
-                error.setIcon(QtWidgets.QMessageBox.Critical)
+                error.setIcon(qtw.QMessageBox.Critical)
                 error.exec_()
 
+    class ProfileGroupBox(qtw.QGroupBox):
+        switch_profile = qtc.pyqtSignal(str)
+
+        def __init__(self):
+            super().__init__()
+            # set Title
+            self.setTitle("Profile")
+
+            # create widgets
+            self.__combo_box = qtw.QComboBox()
+            self.__vstretch_sizePolicy = qtw.QSizePolicy(qtw.QSizePolicy.Minimum, qtw.QSizePolicy.Fixed)
+            self.__vstretch_sizePolicy.setHorizontalStretch(1)
+            self.__combo_box.setSizePolicy(self.__vstretch_sizePolicy)
+            self.__combo_box.currentTextChanged.connect(self.switch_profile)
+            self.__change_button = qtw.QPushButton("Change")
+
+            # layout stuff
+            self.__layout = qtw.QHBoxLayout()
+            self.__layout.addWidget(self.__combo_box)
+            self.__layout.addWidget(self.__change_button)
+
+            self.setLayout(self.__layout)
+
+            self.reload_combobox()
+        
+        def reload_combobox(self):
+            self.__combo_box.clear()
+            self.__combo_box.addItems(cb.get_profile_names())
 
 
 
-class ActionWidget(QtWidgets.QWidget):
-    start = qtc.pyqtSignal()
+class ActionWidget(qtw.QWidget):
+    start = qtc.pyqtSignal(str)
     exit = qtc.pyqtSignal()
 
     def __init__(self):
@@ -190,14 +229,14 @@ class ActionWidget(QtWidgets.QWidget):
         # Widgets 
         self.__scroll_label = self.ScrollLabel()
 
-        self.__progress_bar = QtWidgets.QProgressBar()
+        self.__progress_bar = qtw.QProgressBar()
         self.__progress_bar.setValue(25)
 
-        self.__finish_button = QtWidgets.QPushButton("Finish")
+        self.__finish_button = qtw.QPushButton("Finish")
         self.__finish_button.clicked.connect(self.__on_finish)
         self.__finish_button.setDisabled(True)
 
-        self.__layout = QtWidgets.QGridLayout()
+        self.__layout = qtw.QGridLayout()
         self.__layout.addWidget(self.__scroll_label, 0, 0, 1, 2)
         self.__layout.addWidget(self.__progress_bar, 1, 0, 1, 1)
         self.__layout.addWidget(self.__finish_button, 1, 1, 1, 1)
@@ -210,9 +249,11 @@ class ActionWidget(QtWidgets.QWidget):
     def __on_finish(self):
         self.exit.emit()
 
-    def on_start(self):
+    @qtc.pyqtSlot(str)
+    def on_start(self, profile: str):
         self.__scroll_label.set_text("")
         self.__finish_button.setDisabled(True)
+        self.__thread.set_profile(profile)
         self.__thread.start()
 
     @qtc.pyqtSlot(dict)
@@ -223,25 +264,32 @@ class ActionWidget(QtWidgets.QWidget):
             self.__finish_button.setDisabled(False)
 
             # message
-            messagebox = QtWidgets.QMessageBox()
+            messagebox = qtw.QMessageBox()
             messagebox.setWindowTitle("Finished")
             messagebox.setText("The process finished.")
-            messagebox.setIcon(QtWidgets.QMessageBox.Information)
+            messagebox.setIcon(qtw.QMessageBox.Information)
             messagebox.exec_()
 
     class Thread(qtc.QThread):
         progress = qtc.pyqtSignal(dict)
 
+        def __init__(self):
+            super().__init__()
+            self.__profile = ""
+
+        def set_profile(self, profile):
+            self.__profile = profile
+
         def run(self):
-            for do in cb.start():
+            for do in cb.start(self.__profile):
                 self.progress.emit(do)
 
-    class ScrollLabel(QtWidgets.QScrollArea):
+    class ScrollLabel(qtw.QScrollArea):
         def __init__(self, text: int=""):
             super().__init__()
-            self.__widget = QtWidgets.QWidget()
-            self.__label = QtWidgets.QLabel(text)
-            self.__layout = QtWidgets.QVBoxLayout()
+            self.__widget = qtw.QWidget()
+            self.__label = qtw.QLabel(text)
+            self.__layout = qtw.QVBoxLayout()
             self.__layout.addWidget(self.__label)
             self.__layout.addStretch()  # puts the label at the top
 
