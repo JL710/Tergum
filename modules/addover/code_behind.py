eea7e9426cbe8e158883b8abbbcd4483182876c8
JL710 <git@jl710.de>
2022-08-06 14:07:57 +0200

    addover code behind refactor

--- modules/addover/code_behind.py
+++ modules/addover/code_behind.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 
 
-
+# data handeling
 def load_data() -> dict:
     with open(Path("modules", "addover", "data.json"), "r") as f:
         data = json.load(f)
@@ -18,18 +18,7 @@ def write_data(data: dict):
 # TODO: check profile decorator
 
 
-def get_target(profile: str) -> Path:
-    path_string = load_data()["profiles"][profile]["target"]
-    path = Path(path_string)
-    if path_string == "":
-        return Path.home()
-    return path
-
-def set_target(profile: str, path: str):
-    data = load_data()
-    data["profiles"][profile]["target"] = path
-    write_data(data)
-
+# Profiles
 def get_profile_names() -> list:
     data = load_data()
     return list(data["profiles"].keys())
@@ -48,7 +37,21 @@ def rename_profile(old_profile: str, new_profile: str):
     del data["profiles"][old_profile]
     write_data(data)
 
-def get_payloads(profile: str) -> list:
+# Target
+def get_target(profile: str) -> Path:
+    path_string = load_data()["profiles"][profile]["target"]
+    path = Path(path_string)
+    if path_string == "":
+        return Path.home()
+    return path
+
+def set_target(profile: str, path: str):
+    data = load_data()
+    data["profiles"][profile]["target"] = path
+    write_data(data)
+
+# payload
+def get_payload(profile: str) -> list:
     data = load_data()
     return data["profiles"][profile]["payload"]
 
@@ -69,6 +72,7 @@ def remove_payload(profile: str, payload: str):
         data["profiles"][profile]["payload"].remove(payload)
         write_data(data)
 
+# command
 def get_command() -> str:
     data = load_data()
     return data["command"]
@@ -85,15 +89,16 @@ def reset_command():
     with open(Path("modules", "addover", "data.json"), "w") as f:
         json.dump(data, f)
 
+# start / sync stuff / work stuff
 def start(profile: str):
     # check if destination exists
     if not Path(get_target(profile)).exists():
         yield {"event-message": f"event: Destination {get_target(profile)} does not exist", "percentage": 100, "finished": True}
         return
 
-    percent_factor = 100 / len(get_payloads(profile))
+    percent_factor = 100 / len(get_payload(profile))
 
-    for index_payload, payload in enumerate(get_payloads(profile)):
+    for index_payload, payload in enumerate(get_payload(profile)):
         # check if payload exists
         if not Path(payload).exists():
             yield {"event-message": f"event: Payload {payload} does not exist", "percentage": int(index_payload * percent_factor), "finished": False}
