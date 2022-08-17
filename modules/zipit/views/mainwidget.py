cd5e25e2691e97d93328c4eaa3316701ac94920a
JL710 <git@jl710.de>
2022-08-17 19:42:12 +0200

    zipit widgets
    - load widgets into views.mainwidget.MainWidget
    - create gui widgets for profile stuff

--- modules/zipit/views/mainwidget.py
+++ modules/zipit/views/mainwidget.py
@@ -1,7 +1,27 @@
 from PyQt5 import QtWidgets as qtw
 
+# import widgets
+from .profile import ProfileBox
+from .target import TargetBox
+from .cargo import CargoBox
+from .startbutton import StartButton
+
 
 # mainwidget
 class MainWidget(qtw.QWidget):
     def __init__(self):
         super().__init__()
+
+        # widgets
+        self.__profile_groupbox = ProfileBox()
+        self.__target_groupbox = TargetBox()
+        self.__cargo_groupbox = CargoBox()
+        self.__startbutton = StartButton()
+
+        # layout stuff
+        self.__layout = qtw.QVBoxLayout()
+        self.__layout.addWidget(self.__profile_groupbox)
+        self.__layout.addWidget(self.__target_groupbox)
+        self.__layout.addWidget(self.__cargo_groupbox)
+        self.__layout.addWidget(self.__startbutton)
+        self.setLayout(self.__layout)
--- modules/zipit/views/profile.py
+++ modules/zipit/views/profile.py
@@ -5,4 +5,35 @@ from PyQt5 import QtWidgets as qtw
 class ProfileBox(qtw.QGroupBox):
     def __init__(self):
         super().__init__()
-        
+        self.setTitle("Profile")
+
+        # widgets
+        self.__combobox = qtw.QComboBox()
+
+        minsize_policy = qtw.QSizePolicy(qtw.QSizePolicy.Maximum, qtw.QSizePolicy.Fixed)
+        self.__new_button = qtw.QPushButton("New")
+        self.__new_button.setSizePolicy(minsize_policy)
+        self.__new_button.clicked.connect(self.__on_new)
+        self.__rename_button = qtw.QPushButton("Rename")
+        self.__rename_button.setSizePolicy(minsize_policy)
+        self.__rename_button.clicked.connect(self.__on_rename)
+        self.__delete_button = qtw.QPushButton("Delete")
+        self.__delete_button.setSizePolicy(minsize_policy)
+        self.__delete_button.clicked.connect(self.__on_delete)
+
+        # layout stuff
+        self.__layout = qtw.QHBoxLayout()
+        self.__layout.addWidget(self.__combobox)
+        self.__layout.addWidget(self.__new_button)
+        self.__layout.addWidget(self.__rename_button)
+        self.__layout.addWidget(self.__delete_button)
+        self.setLayout(self.__layout)
+
+    def __on_new(self):
+        print("on_new")
+
+    def __on_rename(self):
+        print("on_rename")
+
+    def __on_delete(self):
+        print("on_delete")
