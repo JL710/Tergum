850cba83de288ad20caa5bc412c35100ff300b06
JL710 <jan.lyncker@freenet.de>
2022-08-20 15:27:35 +0200

    zipit new profile
    - implementation

--- modules/zipit/views/mainwidget.py
+++ modules/zipit/views/mainwidget.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
 
 # import widgets
 from .profile import ProfileBox
@@ -6,14 +7,20 @@ from .target import TargetBox
 from .cargo import CargoBox
 from .startbutton import StartButton
 
+from ..db import DBManager
+
 
 # mainwidget
 class MainWidget(qtw.QWidget):
+
+    load_profile_signal = qtc.pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
+        # signal and slot stuff
 
         # widgets
-        self.__profile_groupbox = ProfileBox()
+        self.__profile_groupbox = ProfileBox(mainwidget=self)
         self.__target_groupbox = TargetBox()
         self.__cargo_groupbox = CargoBox()
         self.__startbutton = StartButton()
@@ -25,3 +32,8 @@ class MainWidget(qtw.QWidget):
         self.__layout.addWidget(self.__cargo_groupbox)
         self.__layout.addWidget(self.__startbutton)
         self.setLayout(self.__layout)
+
+    @qtc.pyqtSlot(str)
+    def new_profile(self, profile: str):
+        DBManager.new_profile(name=profile)
+        self.load_profile_signal.emit(profile)
